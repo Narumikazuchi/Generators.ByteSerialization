73c54816cf870747086b01886e911d136bcb2d16
Narumikazuchi <ganzel.davy@swschwedt.de>
2023-03-30 15:29:39 +0200

    Implemented serializer and interfaces.

---
+++ Narumikazuchi.Generators.ByteSerialization/FixedSerializationSizeAttribute.cs
@@ -0,0 +1,22 @@
+﻿namespace Narumikazuchi.Generators.ByteSerialization;
+
+/// <summary>
+/// Tells the code generator that this strategy will always occupy the same amount of bytes after serialization.
+/// </summary>
+/// <remarks>
+/// Mark <see cref="IByteSerializationStrategy{TSerializable}"/> implementations with this attribute to
+/// optimize the generated serialization code. Has no effect on any other class or struct.
+/// </remarks>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
+public sealed class FixedSerializationSizeAttribute : Attribute
+{
+    /// <summary>
+    /// Tells the code generator that this strategy will always occupy the same amount of bytes after serialization.
+    /// </summary>
+    /// <remarks>
+    /// Mark <see cref="IByteSerializationStrategy{TSerializable}"/> implementations with this attribute to
+    /// optimize the generated serialization code. Has no effect on any other class or struct.
+    /// </remarks>
+    public FixedSerializationSizeAttribute(Int32 size)
+    { }
+}
---
+++ Narumikazuchi.Generators.ByteSerialization/IByteSerializationStrategy.cs
@@ -0,0 +1,32 @@
+﻿namespace Narumikazuchi.Generators.ByteSerialization;
+
+/// <summary>
+/// Provides Methods to serialize a runtime object of type <typeparamref name="TSerializable"/>.
+/// </summary>
+public interface IByteSerializationStrategy<TSerializable>
+{
+    /// <summary>
+    /// Deserializes the contents of the <paramref name="buffer"/> into a runtime object of type <typeparamref name="TSerializable"/>.
+    /// </summary>
+    /// <param name="buffer">The buffer to read from.</param>
+    /// <param name="read">The amount of bytes read from the buffer.</param>
+    /// <returns>The <typeparamref name="TSerializable"/> represented by the buffer.</returns>
+    static public abstract TSerializable Deserialize(ReadOnlySpan<Byte> buffer,
+                                                     out Int32 read);
+
+    /// <summary>
+    /// Calculatees the expected amount of bytes the specified <paramref name="value"/> will occupy once serialized. 
+    /// </summary>
+    /// <param name="value">The value to use as base for calculation.</param>
+    /// <returns>The expected amount of bytes the value will occupy in it's serialized state.</returns>
+    static public abstract Int32 GetExpectedByteSize(TSerializable value);
+
+    /// <summary>
+    /// Serializes the specified <paramref name="value"/> into the <paramref name="buffer"/>.
+    /// </summary>
+    /// <param name="buffer">The buffer into which to store the serialized value.</param>
+    /// <param name="value">The value to serialize.</param>
+    /// <returns>The amount of bytes written to the buffer.</returns>
+    static public abstract Int32 Serialize(Span<Byte> buffer,
+                                           TSerializable value);
+}
