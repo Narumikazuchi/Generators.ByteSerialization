9350e73b38d24b317aa852c298c484f55bfe5d82
Narumikazuchi <ganzel.davy@swschwedt.de>
2023-03-30 15:29:10 +0200

    Implemented attributes for decoration.

---
+++ Narumikazuchi.Generators.ByteSerialization/ByteSerializableAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace Narumikazuchi.Generators.ByteSerialization;
+
+/// <summary>
+/// Generates serialization code for this type.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
+public sealed class ByteSerializableAttribute : Attribute
+{
+    /// <summary>
+    /// Generates serialization code for this type.
+    /// </summary>
+    public ByteSerializableAttribute()
+    { }
+}
---
+++ Narumikazuchi.Generators.ByteSerialization/IngoreForSerializationAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace Narumikazuchi.Generators.ByteSerialization;
+
+/// <summary>
+/// Ignores this member when serializing this object.
+/// </summary>
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false, Inherited = true)]
+public sealed class IngoreForSerializationAttribute : Attribute
+{
+    /// <summary>
+    /// Ignores this member when serializing this object.
+    /// </summary>
+    public IngoreForSerializationAttribute()
+    { }
+}
