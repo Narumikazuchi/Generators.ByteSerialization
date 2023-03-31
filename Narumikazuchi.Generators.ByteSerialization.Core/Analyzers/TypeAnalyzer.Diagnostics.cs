152cdf7fafb7ed9ff44915cb1c3313bfab5516dc
Narumikazuchi <ganzel.davy@swschwedt.de>
2023-04-01 01:32:07 +0200

    Implemented an analyzer.

---
+++ Narumikazuchi.Generators.ByteSerialization.Core/Analyzers/TypeAnalyzer.Diagnostics.cs
@@ -0,0 +1,37 @@
+﻿namespace Narumikazuchi.Generators.ByteSerialization.Analyzers;
+
+public partial class TypeAnalyzer
+{
+    static public Diagnostic CreateNoSerializationForTypeDiagnostic(TypeDeclarationSyntax type,
+                                                                    String membername,
+                                                                    String typename)
+    {
+        return Diagnostic.Create(descriptor: s_NoSerializationForTypeDescriptor,
+                                 location: type.GetLocation(),
+                                 membername,
+                                 typename);
+    }
+
+    static public Diagnostic CreateDuplicateStrategyForTypeDiagnostic(AttributeSyntax attribute,
+                                                                      String typename)
+    {
+        return Diagnostic.Create(descriptor: s_DuplicateStrategyForTypeDescriptor,
+                                 location: attribute.GetLocation(),
+                                 typename);
+    }
+
+    static private readonly DiagnosticDescriptor s_NoSerializationForTypeDescriptor = new(id: "NCG009",
+                                                                                          category: "Code Generation",
+                                                                                          title: "No seralization for member type possible",
+                                                                                          messageFormat: "For the member '{0}' of type '{1}' is no serialization possible. If you own the type consider decorating it with 'ByteSerializable' or otherwise implement a strategy to serialize that type.",
+                                                                                          description: "For the member '{0}' of type '{1}' is no serialization possible. If you own the type consider decorating it with 'ByteSerializable' or otherwise implement a strategy to serialize that type.",
+                                                                                          defaultSeverity: DiagnosticSeverity.Error,
+                                                                                          isEnabledByDefault: true);
+    static private readonly DiagnosticDescriptor s_DuplicateStrategyForTypeDescriptor = new(id: "NCG010",
+                                                                                            category: "Code Generation",
+                                                                                            title: "Multiple strategies defined for the same type",
+                                                                                            messageFormat: "You already defined a strategy for the serialization of type '{0}'.",
+                                                                                            description: "You already defined a strategy for the serialization of type '{0}'.",
+                                                                                            defaultSeverity: DiagnosticSeverity.Error,
+                                                                                            isEnabledByDefault: true);
+}
---
+++ Narumikazuchi.Generators.ByteSerialization.Core/Analyzers/TypeAnalyzer.cs
@@ -0,0 +1,119 @@
+﻿namespace Narumikazuchi.Generators.ByteSerialization.Analyzers;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed partial class TypeAnalyzer : DiagnosticAnalyzer
+{
+    public override void Initialize(AnalysisContext context)
+    {
+        context.EnableConcurrentExecution();
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+        context.RegisterSyntaxNodeAction(action: this.Analyze,
+                                         SyntaxKind.ClassDeclaration,
+                                         SyntaxKind.StructDeclaration,
+                                         SyntaxKind.RecordDeclaration,
+                                         SyntaxKind.RecordStructDeclaration);
+    }
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = new DiagnosticDescriptor[]
+    {
+        s_NoSerializationForTypeDescriptor,
+        s_DuplicateStrategyForTypeDescriptor
+    }.ToImmutableArray();
+
+    private void Analyze(SyntaxNodeAnalysisContext context)
+    {
+        if (context.Node is not TypeDeclarationSyntax type ||
+            context.SemanticModel.Compilation.Options.SyntaxTreeOptionsProvider!.IsGenerated(type.SyntaxTree, CancellationToken.None) is GeneratedKind.MarkedGenerated)
+        {
+            return;
+        }
+
+        SemanticModel semanticModel = context.SemanticModel;
+        INamedTypeSymbol typeSymbol = semanticModel.GetDeclaredSymbol(type);
+        if (typeSymbol is null)
+        {
+            return;
+        }
+
+        if (!typeSymbol.IsSerializable())
+        {
+            return;
+        }
+
+        this.AnalyzeMembers(context: context,
+                            type: type,
+                            typeSymbol: typeSymbol);
+    }
+
+    private void AnalyzeMembers(SyntaxNodeAnalysisContext context,
+                                TypeDeclarationSyntax type,
+                                INamedTypeSymbol typeSymbol)
+    {
+        Dictionary<ITypeSymbol, ITypeSymbol> strategies = __Shared.GetStrategiesFromAttributes(symbol: typeSymbol,
+                                                                                               compilation: context.Compilation,
+                                                                                               type: type,
+                                                                                               duplicates: out Dictionary<ITypeSymbol, List<AttributeData>> duplicates);
+        if (duplicates.Count > 0)
+        {
+            foreach (KeyValuePair<ITypeSymbol, List<AttributeData>> kv in duplicates)
+            {
+                foreach (AttributeData attribute in kv.Value)
+                {
+                    SyntaxReference reference = attribute.ApplicationSyntaxReference!;
+                    AttributeSyntax syntax = (AttributeSyntax)reference.GetSyntax();
+                    context.ReportDiagnostic(CreateDuplicateStrategyForTypeDiagnostic(attribute: syntax,
+                                                                                      typename: kv.Key.ToTypename()));
+                }
+            }
+        }
+
+        ImmutableArray<IFieldSymbol> fields = __Shared.GetFieldsToSerialize(typeSymbol);
+
+        foreach (IFieldSymbol field in fields)
+        {
+            if (strategies.ContainsKey(field.Type))
+            {
+                continue;
+            }
+            else if (Array.IndexOf(array: IntrinsicTypes.SerializedTypes,
+                                   value: field.Type.ToTypename()) > -1)
+            {
+                continue;
+            }
+            else if (field.Type.IsUnmanagedType &&
+                     field.Type.TypeKind is not TypeKind.Pointer &&
+                     field.Type.Name is not "IntPtr"
+                                     and not "UIntPtr")
+            {
+                continue;
+            }
+            else if (field.Type.IsSerializable())
+            {
+                continue;
+            }
+            /*
+            else if (field.Type.IsEnumerable(out ITypeSymbol elementType) &&
+                     (strategies.ContainsKey(elementType!) ||
+                     Array.IndexOf(array: IntrinsicTypes.SerializedTypes,
+                                   value: elementType!.ToTypename()) > -1 ||
+                     elementType!.IsSerializable() ||
+                     elementType!.IsUnmanagedType))
+            {
+                continue;
+            }
+            */
+            else
+            {
+                ISymbol target = field;
+                if (field.AssociatedSymbol is not null)
+                {
+                    target = field.AssociatedSymbol;
+                }
+
+                context.ReportDiagnostic(CreateNoSerializationForTypeDiagnostic(type: type,
+                                                                                membername: target.Name,
+                                                                                typename: field.Type.ToTypename()));
+            }
+        }
+    }
+}
