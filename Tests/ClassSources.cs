5f6be5584545794ad771982c86d0f31eef5447a7
Narumikazuchi <ganzel.davy@swschwedt.de>
2023-05-04 13:12:33 +0200

    Moved out some class sources.

---
+++ Tests/ClassSources.cs
@@ -0,0 +1,43 @@
+﻿namespace Tests;
+
+static public class ClassSources
+{
+    public const String STOREMAP_FILE_SOURCE = @"using System;
+
+public sealed class StoreMap
+{
+    public Guid StoreId { get; set; }
+
+    public Guid ProductId { get; set; }
+}";
+
+    public const String PERSON_FILE_SOURCE = @"using System;
+
+public sealed class Person
+{
+    public String FirstName { get; set; }
+
+    public String LastName { get; set; }
+
+    public DateOnly DateOfBirth { get; set; }
+
+    public Guid CustomerId { get; set; }
+}";
+
+    public const String PRODUCT_FILE_SOURCE = @"using Narumikazuchi.Generators.ByteSerialization;
+using System;
+
+public sealed class Product
+{
+    public Product([SerializeFromMember(nameof(Id))] Guid id)
+    {
+        Id = id;
+    }
+
+    public Guid Id { get; init; }
+
+    public String Name { get; set; }
+
+    public Decimal Price { get; set; }
+}";
+}
