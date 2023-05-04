e48103ae2c62c1d14e96bd8aac1cad970c212f50
Narumikazuchi <ganzel.davy@swschwedt.de>
2023-05-04 11:55:49 +0200

    Implemented additional analyzer to test.

--- Tests/AnalyzerTest.cs
+++ Tests/AnalyzerTest.cs
@@ -8,12 +8,12 @@ using Narumikazuchi.InputOutput;
 
 namespace Tests;
 
-public sealed class AnalyzerTest : CSharpAnalyzerTest<TypeAnalyzer, MSTestVerifier>
+public sealed class InvocationAnalyzerTest : CSharpAnalyzerTest<InvocationAnalyzer, MSTestVerifier>
 {
     static public async Task VerifyAnalyzerAsynchronously(String[] sources,
                                                           params DiagnosticResult[] expected)
     {
-        AnalyzerTest test = new()
+        InvocationAnalyzerTest test = new()
         {
             TestState =
             {
@@ -38,7 +38,7 @@ public sealed class AnalyzerTest : CSharpAnalyzerTest<TypeAnalyzer, MSTestVerifi
                                                           params DiagnosticResult[] expected)
     {
 
-        AnalyzerTest test = new()
+        InvocationAnalyzerTest test = new()
         {
             TestState =
             {
@@ -56,7 +56,68 @@ public sealed class AnalyzerTest : CSharpAnalyzerTest<TypeAnalyzer, MSTestVerifi
         await test.RunAsync(CancellationToken.None);
     }
 
-    public AnalyzerTest()
+    public InvocationAnalyzerTest()
+    {
+        this.SolutionTransforms.Add((solution, projectId) =>
+        {
+            CompilationOptions compilationOptions = solution.GetProject(projectId)!.CompilationOptions!;
+            compilationOptions = compilationOptions.WithSpecificDiagnosticOptions(compilationOptions.SpecificDiagnosticOptions.SetItem("CS1705", ReportDiagnostic.Hidden));
+            solution = solution.WithProjectCompilationOptions(projectId, compilationOptions);
+
+            return solution;
+        });
+    }
+}
+
+public sealed class TypeAnalyzerTest : CSharpAnalyzerTest<TypeAnalyzer, MSTestVerifier>
+{
+    static public async Task VerifyAnalyzerAsynchronously(String[] sources,
+                                                          params DiagnosticResult[] expected)
+    {
+        TypeAnalyzerTest test = new()
+        {
+            TestState =
+            {
+                ReferenceAssemblies = Net7.Assemblies,
+                AdditionalReferences =
+                {
+                    typeof(ByteSerializer).Assembly.Location,
+                    typeof(IStream).Assembly.Location
+                }
+            }
+        };
+
+        foreach (String source in sources)
+        {
+            test.TestState.Sources.Add(source);
+        }
+
+        test.ExpectedDiagnostics.AddRange(expected);
+        await test.RunAsync(CancellationToken.None);
+    }
+    static public async Task VerifyAnalyzerAsynchronously(String source,
+                                                          params DiagnosticResult[] expected)
+    {
+
+        TypeAnalyzerTest test = new()
+        {
+            TestState =
+            {
+                Sources = { source },
+                ReferenceAssemblies = Net7.Assemblies,
+                AdditionalReferences =
+                {
+                    typeof(ByteSerializer).Assembly.Location,
+                    typeof(IStream).Assembly.Location
+                }
+            }
+        };
+
+        test.ExpectedDiagnostics.AddRange(expected);
+        await test.RunAsync(CancellationToken.None);
+    }
+
+    public TypeAnalyzerTest()
     {
         this.SolutionTransforms.Add((solution, projectId) =>
         {
