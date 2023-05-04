d4f0901f8e2aed09316c53cb7109aa58d7f82549
Narumikazuchi <ganzel.davy@swschwedt.de>
2023-05-04 14:14:08 +0200

    Implemented managed with default constructor tests.

---
+++ Tests/CodeGenerator/ManagedWithDefaultConstructor/OneDimensionalArray/Serialize.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.CodeGenerator.ManagedWithDefaultConstructor.OneDimensionalArray;
+
+#pragma warning disable IDE1006 // No need to add postfix 'Asynchronously' here
+[TestClass]
+public class Serialize
+{
+    [TestMethod]
+    public async Task SimpleSerialize()
+    {
+        String source = @"using Narumikazuchi.Generators.ByteSerialization;
+using System;
+
+public class Application
+{
+    static public Byte[] Run(Person[] graph)
+    {
+        return ByteSerializer.Serialize(graph);
+    }
+}";
+
+        await GeneratorTest.VerifySourceGeneratorAsynchronously(new String[] { ClassSources.PERSON_FILE_SOURCE, source }, AssemblySource.ExpectedSource, ExpectedSource.Handler);
+    }
+
+    [TestMethod]
+    public async Task SerializeSafe()
+    {
+        String source = @"using Narumikazuchi.Generators.ByteSerialization;
+using System;
+
+public class Application
+{
+    static public UInt32 Run(Person[] graph)
+    {
+        Byte[] buffer = new Byte[16];
+        return ByteSerializer.Serialize(buffer, graph);
+    }
+}";
+
+        await GeneratorTest.VerifySourceGeneratorAsynchronously(new String[] { ClassSources.PERSON_FILE_SOURCE, source }, AssemblySource.ExpectedSource, ExpectedSource.Handler);
+    }
+
+    [TestMethod]
+    public async Task SerializeUnsafe()
+    {
+        String source = @"using Narumikazuchi.Generators.ByteSerialization;
+using System;
+
+public class Application
+{
+    static public unsafe Byte[] Run(Person[] graph)
+    {
+        Byte[] buffer = new Byte[16];
+        fixed (Byte* pointer = buffer)
+        {
+            ByteSerializer.Serialize(pointer, graph);
+        }
+        return buffer;
+    }
+}";
+
+        await GeneratorTest.VerifySourceGeneratorAsynchronously(new String[] { ClassSources.PERSON_FILE_SOURCE, source }, AssemblySource.ExpectedSource, ExpectedSource.Handler);
+    }
+
+    [TestMethod]
+    public async Task SerializeIOStream()
+    {
+        String source = @"using Narumikazuchi.Generators.ByteSerialization;
+using System;
+using System.IO;
+
+public class Application
+{
+    static public void Run(Person[] graph)
+    {
+        using MemoryStream stream = new MemoryStream();
+        ByteSerializer.Serialize(stream, graph);
+    }
+}";
+
+        await GeneratorTest.VerifySourceGeneratorAsynchronously(new String[] { ClassSources.PERSON_FILE_SOURCE, source }, AssemblySource.ExpectedSource, ExpectedSource.Handler);
+    }
+
+    [TestMethod]
+    public async Task SerializeStream()
+    {
+        String source = @"using Narumikazuchi.Generators.ByteSerialization;
+using Narumikazuchi.InputOutput;
+using System;
+using System.IO;
+
+public class Application
+{
+    static public void Run(Person[] graph)
+    {
+        using MemoryStream stream = new MemoryStream();
+        ByteSerializer.Serialize(stream.AsWriteableStream(), graph);
+    }
+}";
+
+        await GeneratorTest.VerifySourceGeneratorAsynchronously(new String[] { ClassSources.PERSON_FILE_SOURCE, source }, AssemblySource.ExpectedSource, ExpectedSource.Handler);
+    }
+
+    [TestMethod]
+    public async Task SerializeIOStreamAsync()
+    {
+        String source = @"using Narumikazuchi.Generators.ByteSerialization;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class Application
+{
+    static public async Task Run(Person[] graph, CancellationToken cancellationToken)
+    {
+        using MemoryStream stream = new MemoryStream();
+        await ByteSerializer.SerializeAsynchronously(stream, graph, cancellationToken);
+    }
+}";
+
+        await GeneratorTest.VerifySourceGeneratorAsynchronously(new String[] { ClassSources.PERSON_FILE_SOURCE, source }, AssemblySource.ExpectedSource, ExpectedSource.Handler);
+    }
+
+    [TestMethod]
+    public async Task SerializeStreamAsync()
+    {
+        String source = @"using Narumikazuchi.Generators.ByteSerialization;
+using Narumikazuchi.InputOutput;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class Application
+{
+    static public async Task Run(Person[] graph, CancellationToken cancellationToken)
+    {
+        using MemoryStream stream = new MemoryStream();
+        await ByteSerializer.SerializeAsynchronously(stream.AsWriteableStream(), graph, cancellationToken);
+    }
+}";
+
+        await GeneratorTest.VerifySourceGeneratorAsynchronously(new String[] { ClassSources.PERSON_FILE_SOURCE, source }, AssemblySource.ExpectedSource, ExpectedSource.Handler);
+    }
+}
---
+++ Tests/CodeGenerator/ManagedWithDefaultConstructor/TwoDimensionalArray/Deserialize.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.CodeGenerator.ManagedWithDefaultConstructor.TwoDimensionalArray;
+
+#pragma warning disable IDE1006 // No need to add postfix 'Asynchronously' here
+[TestClass]
+public class Deserialize
+{
+    [TestMethod]
+    public async Task DeserializeSafe()
+    {
+        String source = @"using Narumikazuchi.Generators.ByteSerialization;
+using System;
+
+public class Application
+{
+    static public UInt32 Run(Byte[] buffer)
+    {
+        return ByteSerializer.Deserialize<Person[,]>(buffer, out _);
+    }
+}";
+
+        await GeneratorTest.VerifySourceGeneratorAsynchronously(new String[] { ClassSources.PERSON_FILE_SOURCE, source }, AssemblySource.ExpectedSource, ExpectedSource.Handler);
+    }
+
+    [TestMethod]
+    public async Task DeserializeUnsafe()
+    {
+        String source = @"using Narumikazuchi.Generators.ByteSerialization;
+using System;
+
+public class Application
+{
+    static public unsafe UInt32 Run(Byte[] buffer)
+    {
+        UInt32 result;
+        fixed (Byte* pointer = buffer)
+        {
+            result = ByteSerializer.Deserialize<Person[,]>(pointer, out _);
+        }
+        return result;
+    }
+}";
+
+        await GeneratorTest.VerifySourceGeneratorAsynchronously(new String[] { ClassSources.PERSON_FILE_SOURCE, source }, AssemblySource.ExpectedSource, ExpectedSource.Handler);
+    }
+
+    [TestMethod]
+    public async Task DeserializeIOStream()
+    {
+        String source = @"using Narumikazuchi.Generators.ByteSerialization;
+using System;
+using System.IO;
+
+public class Application
+{
+    static public UInt32 Run(Stream stream)
+    {
+        return ByteSerializer.Deserialize<Person[,]>(stream, out _);
+    }
+}";
+
+        await GeneratorTest.VerifySourceGeneratorAsynchronously(new String[] { ClassSources.PERSON_FILE_SOURCE, source }, AssemblySource.ExpectedSource, ExpectedSource.Handler);
+    }
+
+    [TestMethod]
+    public async Task DeserializeStream()
+    {
+        String source = @"using Narumikazuchi.Generators.ByteSerialization;
+using Narumikazuchi.InputOutput;
+using System;
+using System.IO;
+
+public class Application
+{
+    static public UInt32 Run<TStream>(TStream stream)
+        where TStream : IReadableStream
+    {
+        return ByteSerializer.Deserialize<TStream, Person[,]>(stream, out _);
+    }
+}";
+
+        await GeneratorTest.VerifySourceGeneratorAsynchronously(new String[] { ClassSources.PERSON_FILE_SOURCE, source }, AssemblySource.ExpectedSource, ExpectedSource.Handler);
+    }
+
+    [TestMethod]
+    public async Task DeserializeIOStreamAsync()
+    {
+        String source = @"using Narumikazuchi.Generators.ByteSerialization;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class Application
+{
+    static public async Task Run(Stream stream, CancellationToken cancellationToken)
+    {
+        _ = await ByteSerializer.DeserializeAsynchronously<Person[,]>(stream, cancellationToken);
+    }
+}";
+
+        await GeneratorTest.VerifySourceGeneratorAsynchronously(new String[] { ClassSources.PERSON_FILE_SOURCE, source }, AssemblySource.ExpectedSource, ExpectedSource.Handler);
+    }
+
+    [TestMethod]
+    public async Task DeserializeStreamAsync()
+    {
+        String source = @"using Narumikazuchi.Generators.ByteSerialization;
+using Narumikazuchi.InputOutput;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class Application
+{
+    static public async Task Run<TStream>(TStream stream, CancellationToken cancellationToken)
+        where TStream : IReadableStream
+    {
+        _ = await ByteSerializer.DeserializeAsynchronously<TStream, Person[,]>(stream, cancellationToken);
+    }
+}";
+
+        await GeneratorTest.VerifySourceGeneratorAsynchronously(new String[] { ClassSources.PERSON_FILE_SOURCE, source }, AssemblySource.ExpectedSource, ExpectedSource.Handler);
+    }
+}
